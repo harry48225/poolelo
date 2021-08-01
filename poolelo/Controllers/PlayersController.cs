33002586b72bf56792953040180211c99976b872
Rupert Smith <smith.rupert@outlook.com>
2021-08-01 15:29:17 +0100

    Added backend framework

---
+++ poolelo/Controllers/PlayersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using poolelo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace poolelo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayersController : Controller
+    {
+        private readonly IPlayerService _playerService;
+
+        public PlayersController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return await _playerService.GetPlayers();
+        }
+    }
+}
