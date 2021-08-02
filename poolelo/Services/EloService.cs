ab761583b6088dc474d77295de32d3d93d25509e
Harry <harrybest800@gmail.com>
2021-08-02 21:29:46 +0100

    Basic ExpectedScores tests

---
+++ poolelo/Services/EloService.cs
@@ -0,0 +1,12 @@
+namespace poolelo.Services
+{
+    public static class EloService
+    {
+        private static int K_FACTOR = 32;
+
+        public static ComputeExpectedScoreForPlayerWithOpponent(int playerElo, int opponentElo)
+        {
+            double Q_A = Math.
+        }
+    }
+}
